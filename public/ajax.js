c47ee6d8f68cfaef83585295745dd735a9058e64
Koby <kcampbell@bonadmin.com>
2019-12-17 13:01:36 -0700

    test

--- public/ajax.js
+++ public/ajax.js
@@ -1,21 +1,31 @@
-$(document).ready(function(){
-    $("form#calcValues").on('submit', function(e){
-        e.preventDefault();
-        var var1 = $('input[name=var1]').val();
-        var sign = $('input[name=sign]').val();   
-        var var2 = $('input[name=var2]').val();
-        $.ajax({
-            type: 'post',
-            url: '/ajax',
-            data: data{
-  		var1: var1,
-		sign: sign,
-		var2: var2
-	    },
-            dataType: 'text'
-        })
-        .done(function(data){
-            $('h1').html(data.quote);
-        });
-    });
-});
+function search() {
+    var title = document.getElementById("search_params").value;
+    var xhttp = new XMLHttpRequest();
+    var url = 'http://www.omdbapi.com/?t='+title+'&apikey=50bd083';
+
+    xhttp.onload = function() {
+        var data = JSON.parse(this.response);
+        document.getElementById("poster").innerHTML = 
+            "<h1>"+data.Title+" ("+data.Year+")</h1>"+
+            "<img src = "+data.Poster+">"+
+            "<br>"+
+            "______________________________________________"+
+            "<h6>"+data.Genre+"  |  "+data.Released+"</h6>";
+        // var rating = Math.round(data.imdbRating);  (add to line 26 -> '+&frasl;10')
+        var rating = data.imdbRating*10;
+            
+        document.getElementById("info").innerHTML = 
+            "<br><br><br><br><br><br><br><br><br>"+
+            "<h4>IMDB Score: "+rating+"%</h4>"+
+            "<blockquote>"+data.Plot+"</blockquote>"+                    
+            "<h6><strong>Director:</strong> "+data.Director+"</h6>"+
+            "<h6><strong>Writers:</strong> "+data.Writer+"</h6>"+
+            "<h6><strong>Stars:</strong> "+data.Actors+"</h6>";
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+    
+}
+
+
+
